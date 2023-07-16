85c586ee562de519e390f2f84ee8eff097aaf40c
ritesh <RiteshGupta2003>
2023-07-16 09:52:27 -0400

    add update

--- hello.java
+++ hello.java
@@ -1,7 +1,13 @@
-class HelloJava{
+class hello{
     public static void main(String[] args){
         System.out.println("hello world");
 
         // varsha fist code on github
+
+
+        for(Int i = 0;i<10;i++){
+            System.out.println(i);
+        }
+
     }
 }
