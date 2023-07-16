fc1dce6b3b91faeb78d12f4ecb856b9fe2339643
ritesh <RiteshGupta2003>
2023-07-16 10:04:48 -0400

    what is if condition ?

--- hello.java
+++ hello.java
@@ -2,12 +2,11 @@ class hello{
     public static void main(String[] args){
         System.out.println("hello world");
 
-        // varsha fist code on github
+        // varsha task
 
+        // what is if condition in java ? 
 
-        for(Int i = 0;i<10;i++){
-            System.out.println(i);
-        }
+        
 
     }
 }
