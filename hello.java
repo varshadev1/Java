0d976feeb59693fbe6fec95623f8ef4ebd1ebf38
varshadev1 <Vr043753@gmail.com>
2023-07-27 00:05:59 +0530

    for loop code

--- hello.java
+++ hello.java
@@ -5,21 +5,28 @@ class hello{ // class name same as file name
 
         // what is if condition in java ? 
 
-int papa=4;
-int maa=3;
-int bhai=2;
-int me=1;
-if(papa>maa){
-    System.out.println("papa is greater then maa");
-}
-else if(maa>bhai){
-    System.out.println("maa is greater than bhai");
-}
-else if (bhai>me){
-    System.out.println("bhai is greater than me");
-}
-else{
-    System.out.println("i m very smaller in my family");
+// int papa=4;
+// int maa=3;
+// int bhai=2;
+// int me=1;
+// if(papa>maa){
+//     System.out.println("papa is greater then maa");
+// }
+// else if(maa>bhai){
+//     System.out.println("maa is greater than bhai");
+// }
+// else if (bhai>me){
+//     System.out.println("bhai is greater than me");
+// }
+// else{
+//     System.out.println("i m very smaller in my family");
+// }
+// }
+
+
+for(int i=0;i<=10;i++)
+{
+    System.out.println(i);
 }
 }
 }
--- name.java
+++ name.java
@@ -1,5 +1,27 @@
+//for loop
 class name{
     public static void main(String[] args){
-        System.out.println("varsha");
+        //1st code using for loop counting 1 to 10
+        // for(int i=0;i<=10;i++){
+        //     System.out.println(i);
+        // }
+
+        //2nd to find odd number
+        // int n=10;
+        // for(int i=1;i<=n;i++){
+        //     System.out.println(i);
+        //     i=i+1;
+        // }
+
+
+        //3rd to find out even number
+        int n=10;
+        for(int i=1;i<=n;i++){
+            if(i%2==0){
+            System.out.println(i);
+            }
+            
+        }
+        
     }
 }
