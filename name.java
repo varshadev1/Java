17760f262c7715fa90741ba6b12a544ecb51f0f9
varshadev1 <Vr043753@gmail.com>
2023-07-27 22:07:05 +0530

    for loop code

--- name.java
+++ name.java
@@ -1,13 +1,13 @@
-//for loop
 class name{
     public static void main(String[] args){
+        //for loop
         //1st code using for loop counting 1 to 10
         // for(int i=0;i<=10;i++){
         //     System.out.println(i);
         // }
 
         //2nd to find odd number
-        // int n=10;
+        // int n=100;
         // for(int i=1;i<=n;i++){
         //     System.out.println(i);
         //     i=i+1;
@@ -15,13 +15,41 @@ class name{
 
 
         //3rd to find out even number
-        int n=10;
-        for(int i=1;i<=n;i++){
-            if(i%2==0){
-            System.out.println(i);
-            }
+        // int n=10;
+        // for(int i=1;i<=n;i++){
+        //     if(i%2==0){
+        //     System.out.println(i);
+        //     }
             
-        }
+        // }
         
+//---------------------------------------
+       //while loop
+       //to find summation from 1 to 10
+    //    int x=1;
+    //    int sum=0;
+    //    while(x<=10){
+    //     sum=sum+x;
+    //     x++;
+    //    }
+    //    System.out.println("summation: " + sum);
+
+
+    //reverse of counting from 10 to 1
+    // int i=10;
+    // while(i>=1){
+    //     System.out.println(i);
+    //     i--;
+    // }
+
+
+     int num=2, i=1;
+    while(i<=10)
+    {
+        System.out.printf("%d * %d = %d\n",num,i,num*i);
+        i++;   
+         }
+
+    
     }
 }
