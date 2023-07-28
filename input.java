7574a3021677f52b1d876c4902c93d696d1f8f9b
varshadev1 <Vr043753@gmail.com>
2023-07-28 22:38:27 +0530

    user input code 2

---
+++ input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class input{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter 1st number ");
+        int a= s.nextInt();
+
+    }
+}
