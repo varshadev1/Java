167698073d55558cd53c4dc6f978cc8bdc57bcff
varshadev1 <Vr043753@gmail.com>
2023-08-09 23:58:15 +0530

    output format

--- mypc.java
+++ mypc.java
@@ -9,11 +9,36 @@ public class mypc {
         double d = scan.nextDouble();
         String s= scan.nextLine()+" ";
         s=scan.nextLine();
-
-        // Write your code here.
-
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
         System.out.println("Int: " + i);
     }
 }
+
+
+// Note: If you use the nextLine() method immediately following the nextInt() method, recall that nextInt() reads integer tokens; because of this, the last newline character for that line of integer input is still queued in the input buffer and the next nextLine() will be reading the remainder of the integer line (which is empty).
+
+
+
+
+
+// import java.util.Scanner;
+
+// public class Solution {
+
+//     public static void main(String[] args) {
+//             Scanner sc = new Scanner(System.in);
+//             System.out.println("================================");
+//             for(int i=0;i<3;i++){
+//                 String s1 = sc.next();
+//                 int x = sc.nextInt();
+//                 //Complete this line
+//                 System.out.printf("%-15s%03d\n",s1, x);
+//             }
+//             System.out.println("================================");
+
+//     }
+// }
+
+
+
