f3bfda5cfb47d7ab97ad506d7dbdebc1bc3b3ffc
varshadev1 <Vr043753@gmail.com>
2023-08-10 00:02:35 +0530

    user format in terminal

--- mypc.java
+++ mypc.java
@@ -14,31 +14,3 @@ public class mypc {
         System.out.println("Int: " + i);
     }
 }
-
-
-// Note: If you use the nextLine() method immediately following the nextInt() method, recall that nextInt() reads integer tokens; because of this, the last newline character for that line of integer input is still queued in the input buffer and the next nextLine() will be reading the remainder of the integer line (which is empty).
-
-
-
-
-
-// import java.util.Scanner;
-
-// public class Solution {
-
-//     public static void main(String[] args) {
-//             Scanner sc = new Scanner(System.in);
-//             System.out.println("================================");
-//             for(int i=0;i<3;i++){
-//                 String s1 = sc.next();
-//                 int x = sc.nextInt();
-//                 //Complete this line
-//                 System.out.printf("%-15s%03d\n",s1, x);
-//             }
-//             System.out.println("================================");
-
-//     }
-// }
-
-
-
