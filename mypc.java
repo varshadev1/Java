a8add8055580551cfedf828b5a53b7d73696a95b
varshadev1 <Vr043753@gmail.com>
2023-08-11 22:02:31 +0530

    else if condition

--- mypc.java
+++ mypc.java
@@ -1,27 +1,31 @@
+import java.util.*;
 import java.util.Scanner;
-
-public class mypc
+class mypc
 {
 	public static void main (String[] args)
 	{
 		Scanner read = new Scanner(System.in);
-		
 		int t = read.nextInt();
 		for(int i=0; i<t; i++)
 		{
-    		int x = read.nextInt();
-    		int y = read.nextInt();
-    		int a = read.nextInt();
-
-    		if(a>=x && a < y){
-    		    System.out.println("YES");
-    		}
-    		else {
-    		    System.out.println("NO");
+    		int N = read.nextInt();
+    		int A = read.nextInt();
+    		int B = read.nextInt();
+            if(N%A == 0 && N%B == 0){
+                System.out.println("N is divisible by A and B");
+            }
+            else if(N%A == 0){
+                System.out.println("N is divisible by only A");
+            }
+            else if(N%B == 0){
+                System.out.println("N is divisible by only B");
+            }
+            
+            // The last statement could have been an 'else' statement
+            // elif condition used to show usage of 'and' statement
+    		else if(N%A != 0 && N%B != 0){
+    		    System.out.println("N is divisible by neither A nor B");
     		}
 		}
 	}
 }
-
-
-
