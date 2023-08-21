40e43e2721145e2ab62ac3464cc9306aaa6d9de8
varshadev1 <Vr043753@gmail.com>
2023-08-21 23:48:43 +0530

    integers with space

---
+++ maths.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class maths
+{//all the entered digits of integers,each seperated by space
+     public static void main(String[] args)
+    {
+        Scanner read = new Scanner(System.in);
+        int t = read.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = read.nextInt();
+            ArrayList<Integer> final_ans = new ArrayList<>();
+            while (n>0) {
+                final_ans.add(0, n % 10);
+                n = n / 10;
+            }
+            for (int j=0; j<final_ans.size(); j++) {
+                System.out.print(final_ans.get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
